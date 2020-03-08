dbee8e75e8f7d24b6e246afca3dfec76323948c7
Matthew Gleich <matthewgleich@gmail.com>
2020-03-08 14:53:15 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+
+import 'package:scouting_app/screens/aboutUs.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+        fontFamily: "Nunito",
+      ),
       home: Home(),
       debugShowCheckedModeBanner: false,
     );
@@ -21,11 +29,88 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("501 Scouting"),
+        leading: Padding(
+          padding: const EdgeInsets.only(
+            top: 10,
+            bottom: 10,
+          ),
+          child: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.settings),
+          ),
+        ),
+        title: Text(
+          "FRC 501 Scouting",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         centerTitle: true,
+        // backgroundColor: Colors.white,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 8.0,
+              top: 8.0,
+              bottom: 8.0,
+            ),
+            child: FloatingActionButton(
+              onPressed: null,
+              child: Icon(
+                Icons.bluetooth,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (_) {
+                    return AboutUsScreen();
+                  },
+                ),
+              );
+            },
+            child: Image(
+              image: AssetImage('assets/images/black-powerKnights-logo.png'),
+            ),
+          ),
+          SizedBox(width: 10)
+        ],
       ),
       body: Center(
-        child: Text("Hello!"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: AnimationConfiguration.toStaggeredList(
+            duration: const Duration(
+              seconds: 1,
+              milliseconds: 50,
+            ),
+            childAnimationBuilder: (widget) => SlideAnimation(
+              horizontalOffset: 120.0,
+              child: FadeInAnimation(child: widget),
+            ),
+            children: <Widget>[
+              Text(
+                "👋 Hello!",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                "Welcome to Team 501 the PowerKnights'\n Scouting Application",
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "\nClick on the Settings button above to get started.",
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
