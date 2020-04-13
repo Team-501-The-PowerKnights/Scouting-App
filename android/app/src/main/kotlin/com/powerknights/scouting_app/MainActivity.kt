4e1c86c75d736d72335c74b70f7fdf18b98db1b8
Matthew Gleich <matthewgleich@gmail.com>
2020-04-13 16:09:10 -0400

    Re-initialize application

---
+++ android/app/src/main/kotlin/com/powerknights/scouting_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.powerknights.scouting_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:scouting_app/screens/setup/setup.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(ScoutingApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
+class ScoutingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.black,
-        accentColor: Colors.white,
-        fontFamily: "Nunito",
-      ),
-      home: Home(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SetupStartScreen();
+    return Container();
   }
 }
