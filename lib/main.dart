10fc1c5318374100adff038174efb61f35a98e74
Matthew Gleich <matthewgleich@gmail.com>
2020-04-14 16:25:32 -0400

    Create greeting screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,44 @@
+// Flutter imports
 import 'package:flutter/material.dart';
+
+// Package imports
+import 'package:shared_preferences/shared_preferences.dart';
+
+// Project imports
+import 'package:scouting_app/routes/home/home.dart';
+import 'package:scouting_app/routes/setup/greeting.dart';
+import 'package:scouting_app/themes.dart';
 
 void main() => runApp(ScoutingApp());
 
 class ScoutingApp extends StatelessWidget {
+  /// If the user has completed the setup process
+  Future<bool> _checkSetup() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('setup') ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final themes = Themes();
+    return FutureBuilder(
+      future: _checkSetup(),
+      builder: (context, snapshot) =>
+          snapshot.connectionState == ConnectionState.done
+              ? MaterialApp(
+                  title: 'Scouting App',
+                  debugShowCheckedModeBanner: false,
+                  theme: themes.lightTheme,
+                  darkTheme: themes.darkTheme,
+                  initialRoute: snapshot.data
+                      ? HomeRoute.routeName
+                      : SetupGreetingRoute.routeName,
+                  routes: {
+                    HomeRoute.routeName: (ctx) => HomeRoute(),
+                    SetupGreetingRoute.routeName: (ctx) => SetupGreetingRoute()
+                  },
+                )
+              : CircularProgressIndicator(),
+    );
   }
 }
---
+++ lib/routes/setup/greeting.dart
@@ -0,0 +1,76 @@
+// Flutter imports
+import 'package:flutter/material.dart';
+
+// Package imports
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+
+class SetupGreetingRoute extends StatelessWidget {
+  static const routeName = '/setup/greeting';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimationLimiter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: AnimationConfiguration.toStaggeredList(
+              duration: const Duration(seconds: 1),
+              childAnimationBuilder: (widget) => SlideAnimation(
+                verticalOffset: 50,
+                child: FadeInAnimation(
+                  child: widget,
+                ),
+              ),
+              children: <Widget>[
+                const Text(
+                  "👋 Hello!",
+                  style: TextStyle(
+                    fontSize: 40,
+                  ),
+                ),
+                const SizedBox(height: 50),
+                const Text(
+                  "Lets get you setup",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                const SizedBox(
+                  height: 70,
+                ),
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                  onPressed: () {},
+                  color: Colors.green,
+                  splashColor: Colors.green,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Start Setup",
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Icon(
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
